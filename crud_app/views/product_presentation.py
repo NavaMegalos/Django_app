affeb43f9a47b207257b595504819f1255c17bb8
M3galos <juannava67@live.com>
2025-02-08 17:29:47 -0600

    New functionality added to brand and product presentation, both of 'em now have the crud operations

--- crud_app/views/product_presentation.py
+++ crud_app/views/product_presentation.py
@@ -1,25 +1,83 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, JsonResponse
+from django.urls import reverse
+from django.contrib import messages
+
+from ..models import ProductPresentation
+from ..forms import ProductPresentationForm
 
 def show(request):
-    context = {}
-    render(request, "product_presentaion/show.html", context)
-    
+    presentations = ProductPresentation.objects.all()
+    form = ProductPresentationForm()
+    context = {"presentations": presentations, "form": form}
+    return render(request, "product_presentation/show.html", context)
+
+
 def get(request):
-    context = {}
-    render(request, "product_presentaion/show.html", context)
+    if not request.GET:
+        return
+
+    data = {}
+    id = request.GET["id"]
+    try:
+        presentation = ProductPresentation.objects.get(id=id)
+        data = {
+            "id": presentation.id,
+            "name": presentation.name,
+        }
+    except Presentation.DoesNotExist:
+        data = {"error": "presentation not Found"}
+
+    return JsonResponse(data)
+
 
 def create(request):
     context = {}
-    render(request, "product_presentaion/show.html", context)
-    
-    
+    if request.method == "POST":
+        presentation = ProductPresentationForm(request.POST)
+        if presentation.is_valid():
+            presentation.save()
+            return HttpResponseRedirect(
+                reverse("crud:show_presentation"),
+                messages.add_message(
+                    request, messages.SUCCESS, "Nueva Presentacion Agregada."
+                ),
+            )
+
+    return render(request, "product_presentation/show.html", context)
+
+
 def delete(request):
-    context = {}
-    render(request, "product_presentaion/show.html", context)
-    
-    
+    if not request.POST:
+        return
+
+    id = request.POST["id_presentation"]
+    try:
+        presentation = ProductPresentation.objects.get(id=id)
+        presentation.delete()
+    except Exception as e:
+        msg = messages.add_message(request, messages.ERROR, f"error: {e}")
+        return HttpResponseRedirect(reverse("crud:show_presentation"), msg)
+
+    msg = messages.add_message(request, messages.INFO, "El registro se ha eliminado!")
+    return HttpResponseRedirect(reverse("crud:show_presentation"), msg)
+
+
 def update(request):
-    context = {}
-    render(request, "product_presentaion/show.html", context)
-    
+    if not request.POST:
+        return
+
+    id = request.POST["id_presentation"]
+    name = request.POST["name_presentation"]
+    try:
+        presentation = ProductPresentation.objects.get(id=id)
+        presentation.name = name
+        presentation.save()
+    except Exception as e:
+        msg = messages.add_message(request, messages.ERROR, f"error: {e}")
+        return HttpResponseRedirect(reverse("crud:show_presentation"), msg)
+
+    msg = messages.add_message(
+        request, messages.SUCCESS, "El registro se ha actualizado con exito!"
+    )
+    return HttpResponseRedirect(reverse("crud:show_presentation"), msg)
